e9786cda979a13b185b2844d3ed8a68dd69db34f
safumulatu <safumulatu9@gmail.com>
2024-11-04 11:25:51 +0300

    user model

---
+++ server/model/User/User.js
@@ -0,0 +1,85 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now(),
+    },
+    isVerfied: {
+      type: Boolean,
+      default: false,
+    },
+    accountLevel: {
+      type: String,
+      enum: ["bronze", "silver", "gold"],
+      default: "bronze",
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverImage: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    notificationPreferences: {
+      email: {
+        type: String,
+        default: true,
+      },
+      //..other notifications (sms)
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+    profileViewers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    blockedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    likedPosta: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    passwordResetToken: {
+      type: String,
+    },
+    passwordResetExpires: {
+      type: Date,
+    },
+    accountVerificationToken: {
+      type: String,
+    },
+    accountVerificationExpires: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+//! compile schema
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
