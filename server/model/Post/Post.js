a42b4223c3f5449f5f53c0d86788d1ccffa0f727
safumulatu <safumulatu9@gmail.com>
2024-11-04 11:34:46 +0300

    post Model

---
+++ server/model/Post/Post.js
@@ -0,0 +1,85 @@
+const mongoose = require("mongoose");
+
+//! Post schema
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    claps: {
+      type: Number,
+      default: 0,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    shares: {
+      type: Number,
+      default: 0,
+    },
+
+    postViews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Category",
+    },
+    shedduledPublished: {
+      type: Date,
+      default: null,
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    // toJSON: {
+    //   virtuals: true,
+    // },
+    // toObject: {
+    //   virtuals: true,
+    // },
+  }
+);
+
+//compile schema to  model
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
