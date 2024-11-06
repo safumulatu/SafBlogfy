28fc615f530de8abf3a37c578d93e168dad4c98d
safumulatu <safumulatu9@gmail.com>
2024-11-06 13:03:50 +0300

    blocking and un blocking the user

--- server/controllers/users/UserCtrl.js
+++ server/controllers/users/UserCtrl.js
@@ -63,3 +63,62 @@ exports.getProfile = asyncHandler(async (req, res) => {
   const { password: hashedPassword, ...userInfo } = user._doc;
   res.json({ userInfo });
 });
+//@desc   Block user
+//@route  PUT /api/v1/users/block/:userIdToBlock
+//@access Private
+exports.blockUser = asyncHandler(async (req, res) => {
+  //! find the user to block
+  const userIdToBlock = req.params.userIdToBlock;
+  const userToBlock = await User.findById(userIdToBlock);
+  if (!userToBlock) {
+    throw new Error("user to block not found");
+  }
+
+  //@ user who is blocking the user
+  const userBlocking = req.userAuth._id;
+  // check if user is blocking him self
+  if (userIdToBlock.toString() === userBlocking) {
+    throw new Error("You can't block yourself");
+  }
+  //find the curreent user
+  const currentUser = await User.findById(userBlocking);
+  // check if user is already blocked
+  if (currentUser?.blockedUsers?.includes(userIdToBlock)) {
+    throw new Error("User is already blocked");
+  }
+  //push the user to be blocked in the array of the current user
+  currentUser?.blockedUsers?.push(userIdToBlock);
+  await currentUser.save();
+  res.status(200).json({
+    message: "User blocked successfully",
+    status: "success",
+  });
+});
+
+//@desc   unBlock user
+//@route  PUT /api/v1/users/unblock/:userIdToUnBlock
+//@access Private
+
+exports.unblockUser = asyncHandler(async (req, res) => {
+  const userIdToUnBlock = req.params.userIdToUnBlock;
+  const userToUnBlock = await User.findById(userIdToUnBlock);
+  if (!userToUnBlock) {
+    return res.status(404).json({ message: "User to be unblocked not found" });
+  }
+
+  const userUnBlocking = req.userAuth._id;
+  const currentUser = await User.findById(userUnBlocking);
+
+  if (!currentUser.blockedUsers.includes(userIdToUnBlock)) {
+    return res.status(400).json({ message: "User is not blocked" });
+  }
+
+  currentUser.blockedUsers = currentUser.blockedUsers.filter(
+    (id) => id.toString() !== userIdToUnBlock.toString()
+  );
+  await currentUser.save();
+  res.json({
+    status: "success",
+    message: "User unblocked successfully",
+  });
+});
--- server/routes/users/usersRouter.js
+++ server/routes/users/usersRouter.js
@@ -3,14 +3,26 @@ const {
   register,
   login,
   getProfile,
+  blockUser,
+  unblockUser,
 } = require("../../controllers/users/UserCtrl");
 const isLoggin = require("../../middileware/isLoggedIn");
 const userRouter = express.Router();
 
-// Import User model
+//! register
 userRouter.post("/register", register);
+
+//! login
 userRouter.post("/login", login);
+
+//! get profile
 userRouter.get("/profile/:id", isLoggin, getProfile);
+
+//! block user
+userRouter.put("/block/:userIdToBlock", isLoggin, blockUser);
+
+//! unblock user
+userRouter.put("/unblock/:userIdTounBlock", isLoggin, unblockUser);
 
 //? export userRouter
 module.exports = userRouter;
